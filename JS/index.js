fadf319c3554abaa3e21c21362015f409088e6a4
Kefyalew <koutlook19@gmail.com>
2024-08-08 13:38:07 +0300

    products file

---
+++ JS/index.js
@@ -0,0 +1,46 @@
+
+let productsHtml = '';
+products.forEach(product => {
+ productsHtml+= `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img
+              class="product-image"
+              src="${product.image}"
+              alt=""
+            />
+          </div>
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+          <div class="ratings">
+            <img
+              class="rating-icons"
+              src="images/ratings/rating-${product.rating.stars*10}.png"
+              alt=""
+            />
+            <div class="product-rating-count link-primary">${product.rating.count}</div>
+          </div>
+          <div class="product-price">$${(product.PriceCents/100).toFixed(2)}</div>
+          <div class="select-number">
+            <select name="select-number" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div class="product-spacer"></div>
+          <div class="add-to-cart-button">
+            <button class="add-cart button-primary">Add to Cart</button>
+          </div>
+        </div>
+  `
+})
+document.querySelector('.js-products-grid').innerHTML = productsHtml;
---
+++ data/products.js
@@ -0,0 +1,115 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  PriceCents: 1090
+},
+{
+  image: 'images/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127
+  },
+  PriceCents: 2095
+},
+{
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  PriceCents: 799
+  }, {
+    image: 'images/products/black-2-slot-toaster.jpg',
+    name: '2 slot toaster-black',
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    PriceCents: 1899
+  }, {
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-non-stick-baking-set.webp',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  },{
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 piece white dinner plate set',
+    rating:{
+  stars: '4',
+  count:'37'
+  },
+PriceCents:'2067'
+  }];
