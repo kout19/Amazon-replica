dce71d88de700e9f1d1013b4fc8f4f823cb40d39
Kefyalew <koutlook19@gmail.com>
2024-08-18 01:29:31 +0300

    check out  create html using js

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,17 @@
-export const cart = [];
+import { products } from "../data/products.js";
+let quantityNumber = 0;
+export const cart = [{
+  productId: '15b6fc6f-3ddt34233-kk4452sf-1',
+  quantity:quantityNumber
+}, {
+  productId: '15b6fc6f-3ddt34233-kk4452sf-2',
+  quantity:quantityNumber
+  }];
 export function addToCartFunction(productId) {
   const quantitySelector = document.querySelector(`.js-select-quantity-${productId}`);
    const addedToCart = document.querySelector(`.added-to-cart-${productId}`);
-   let quantitySelectorValue = Number(quantitySelector.value);
+  let quantitySelectorValue = Number(quantitySelector.value);
+  quantityNumber = quantitySelectorValue;
   let matchingItem ;
     cart.forEach((cartItem) => {
       if (productId === cartItem.productId) {
