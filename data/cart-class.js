de84e27888303cb0090298912bf84dfab296245b
Kefyalew <koutlook19@gmail.com>
2024-10-07 15:20:28 +0300

    creating class for cart

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,11 +1,14 @@
-import { loadFromStorage } from "./cart.js";
-import { products } from "./products.js";
-let quantityNumber = 0;
-function Cart(localStorageKey) {  // C is capital b/c we are generating objects
-    const cart = {
-    cartItems: undefined,
-    loadFromStorage() {
-      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+let quantityNumber = 0; 
+
+class Cart{
+  cartItems = undefined;
+  localStorageKey = undefined;
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+  loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
       if (!this.cartItems) {
         this.cartItems = [{
           productId: '15b6fc6f-3ddt34233-kk4452sf-1',
@@ -17,12 +20,14 @@ function Cart(localStorageKey) {  // C is capital b/c we are generating objects
           deliveryOptionId: '2'
         }];
       }
-    },
+  }
+  
   saveToStorage() {
     localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
-      },
+  }
   
-    addToCartFunction(productId) {
+
+  addToCartFunction(productId) {
     const quantitySelector = document.querySelector(`.js-select-quantity-${productId}`);
     const addedToCart = document.querySelector(`.added-to-cart-${productId}`);
     let quantitySelectorValue = Number(quantitySelector.value);
@@ -50,8 +55,9 @@ function Cart(localStorageKey) {  // C is capital b/c we are generating objects
         addedToCart.classList.remove("item-added");
       }, 2000);
     this.saveToStorage();
-    },
-    removeFromCart(productId) {
+  }
+  
+   removeFromCart(productId) {
     const newCart = [];
     this.cartItems.forEach((cartItem) => {
       if(cartItem.productId !== productId){
@@ -60,7 +66,8 @@ function Cart(localStorageKey) {  // C is capital b/c we are generating objects
     })
     this.cartItems = newCart;
     this.saveToStorage();
-    },
+  }
+  
     updateDeliveryOption(productId, deliveryOptionId) {
     let matchingItem ;
       this.cartItems.forEach((cartItem) => {
@@ -71,12 +78,9 @@ function Cart(localStorageKey) {  // C is capital b/c we are generating objects
     matchingItem.deliveryOptionId = deliveryOptionId;
     this.saveToStorage();
   }
-  };
-  return cart;
 }
-let cart = Cart('cart-oop');
-let bussinesCart = Cart('cart-bussines');
-bussinesCart.loadFromStorage();
-cart.loadFromStorage();
+
+let cart = new Cart('cart-oop');
+let bussinesCart = new Cart('cart-bussines');
 console.log(cart)
 console.log(bussinesCart)
